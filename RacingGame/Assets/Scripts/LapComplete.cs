b966e4fe9be25c20ccc00cdf48e8935d4f08bda8
JWalczak <jwalczak33@gmail.com>
2018-05-05 14:29:44 +0200

    Time triggers added

---
+++ RacingGame/Assets/Scripts/LapComplete.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapComplete : MonoBehaviour {
+
+	public GameObject LapCompleteTrig;
+	public GameObject HalfLapTrig;
+
+	public GameObject MinuteDisplay;
+	public GameObject SecondDisplay;
+	public GameObject MilisecondDisplay;
+
+	public GameObject LapTimeBox;
+
+	void OnTriggerEnter(){
+
+		if (LapTimeManager.SecondCount <= 9) {
+			SecondDisplay.GetComponent<Text> ().text = "0" + LapTimeManager.SecondCount + ".";
+		} else {
+			SecondDisplay.GetComponent<Text> ().text = "" + LapTimeManager.SecondCount + ".";
+		}
+	
+		if (LapTimeManager.MinuteCount <= 9) {
+			MinuteDisplay.GetComponent<Text> ().text = "0" + LapTimeManager.MinuteCount + ":";
+		} else {
+			MinuteDisplay.GetComponent<Text> ().text = "" + LapTimeManager.MinuteCount + ":";
+		}
+
+		MilisecondDisplay.GetComponent<Text> ().text = "" + LapTimeManager.MilisecondCount;
+
+		LapTimeManager.MinuteCount = 0;
+		LapTimeManager.SecondCount= 0;
+		LapTimeManager.MilisecondCount = 0;
+
+		HalfLapTrig.SetActive (true);
+		LapCompleteTrig.SetActive (false);
+	}
+
+}
---
+++ RacingGame/Assets/Scripts/LapTimeManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapTimeManager : MonoBehaviour {
+
+	public static int MinuteCount;
+	public static int SecondCount;
+	public static float MilisecondCount;
+	public static string MilisecondDisplay;
+
+	public GameObject MinuteBox;
+	public GameObject SecondBox;
+	public GameObject MilisecondBox;
+
+	void Update () {
+		MilisecondCount += Time.deltaTime * 10;		
+		MilisecondDisplay = MilisecondCount.ToString ("F0");
+		MilisecondBox.GetComponent<Text> ().text = "" + MilisecondDisplay;
+
+		if (MilisecondCount >= 10) {
+			MilisecondCount = 0;
+			SecondCount++;
+		}	
+
+		if (SecondCount <= 9) {
+			SecondBox.GetComponent<Text> ().text = "0" + SecondCount + ".";
+		} else {
+			SecondBox.GetComponent<Text> ().text = "" + SecondCount + ".";
+		}
+
+		if (SecondCount >= 60) {
+			SecondCount = 0;
+			MinuteCount++;		
+		}
+
+		if (MinuteCount <= 9) {
+			MinuteBox.GetComponent<Text> ().text = "0" + MinuteCount + ":";
+		} else {
+			MinuteBox.GetComponent<Text> ().text = "" + MinuteCount + ":";
+		}
+	}
+}
