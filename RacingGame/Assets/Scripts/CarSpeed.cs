f306d8a122f90a28eda8c0790b1a884083c3a689
Wojciech Pszonak <wojciech.pszonak@gmail.com>
2018-05-11 20:08:53 +0200

    Add speedometer, fix car settings, fix best time display

---
+++ RacingGame/Assets/Scripts/CarSpeed.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarSpeed : MonoBehaviour {
+
+    public Rigidbody car;
+
+    public Text speedDisplay;
+
+    private int speed;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        speed = (int)(car.velocity.magnitude * 3.6);
+        speedDisplay.text = speed.ToString();
+	}
+}
--- RacingGame/Assets/Scripts/LapComplete.cs
+++ RacingGame/Assets/Scripts/LapComplete.cs
@@ -1,41 +1,66 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LapComplete : MonoBehaviour {
+public class LapComplete : MonoBehaviour
+{
 
-	public GameObject LapCompleteTrig;
-	public GameObject HalfLapTrig;
+    public GameObject LapCompleteTrig;
+    public GameObject HalfLapTrig;
 
-	public GameObject MinuteDisplay;
-	public GameObject SecondDisplay;
-	public GameObject MilisecondDisplay;
+    public GameObject BestMinuteDisplay;
+    public GameObject BestSecondDisplay;
+    public GameObject BestMillisecondDisplay;
 
-	public GameObject LapTimeBox;
+    private int BestMinuteCount = int.MaxValue;
+    private int BestSecondCount = int.MaxValue;
+    private float BestMillisecondCount = int.MaxValue;
 
-	void OnTriggerEnter(){
+    public GameObject LapTimeBox;
 
-		if (LapTimeManager.SecondCount <= 9) {
-			SecondDisplay.GetComponent<Text> ().text = "0" + LapTimeManager.SecondCount + ".";
-		} else {
-			SecondDisplay.GetComponent<Text> ().text = "" + LapTimeManager.SecondCount + ".";
-		}
-	
-		if (LapTimeManager.MinuteCount <= 9) {
-			MinuteDisplay.GetComponent<Text> ().text = "0" + LapTimeManager.MinuteCount + ":";
-		} else {
-			MinuteDisplay.GetComponent<Text> ().text = "" + LapTimeManager.MinuteCount + ":";
-		}
+    void OnTriggerEnter()
+    {
 
-		MilisecondDisplay.GetComponent<Text> ().text = "" + LapTimeManager.MilisecondCount;
+        if (LapTimeManager.MinuteCount < BestMinuteCount
+            || (LapTimeManager.MinuteCount == BestMinuteCount
+                && (LapTimeManager.SecondCount < BestSecondCount
+                    || (LapTimeManager.SecondCount == BestSecondCount
+                        && LapTimeManager.MilisecondCount < BestMillisecondCount))))
+        {
+            BestMinuteCount = LapTimeManager.MinuteCount;
+            BestSecondCount = LapTimeManager.SecondCount;
+            BestMillisecondCount = LapTimeManager.MilisecondCount;
 
-		LapTimeManager.MinuteCount = 0;
-		LapTimeManager.SecondCount= 0;
-		LapTimeManager.MilisecondCount = 0;
 
-		HalfLapTrig.SetActive (true);
-		LapCompleteTrig.SetActive (false);
-	}
+            if (BestSecondCount <= 9)
+            {
+                BestSecondDisplay.GetComponent<Text>().text = "0" + BestSecondCount + ".";
+            }
+            else
+            {
+                BestSecondDisplay.GetComponent<Text>().text = "" + BestSecondCount + ".";
+            }
+
+            if (BestMinuteCount <= 9)
+            {
+                BestMinuteDisplay.GetComponent<Text>().text = "0" + BestMinuteCount + ":";
+            }
+            else
+            {
+                BestMinuteDisplay.GetComponent<Text>().text = "" + BestMinuteCount + ":";
+            }
+
+            BestMillisecondDisplay.GetComponent<Text>().text = "" + BestMillisecondCount;
+
+        }
+
+        LapTimeManager.MinuteCount = 0;
+        LapTimeManager.SecondCount = 0;
+        LapTimeManager.MilisecondCount = 0;
+
+        HalfLapTrig.SetActive(true);
+        LapCompleteTrig.SetActive(false);
+    }
 
 }
