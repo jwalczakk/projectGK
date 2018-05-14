146c1b84c70b000b401bde5a6f911dcd9c275416
Wojciech Pszonak <wojciech.pszonak@gmail.com>
2018-05-14 23:16:22 +0200

    Add camera look-back option

--- RacingGame/Assets/Scripts/ChangeCamera.cs
+++ RacingGame/Assets/Scripts/ChangeCamera.cs
@@ -16,7 +16,15 @@ public class ChangeCamera : MonoBehaviour
 
     public GameObject firstPersonCamera;
 
+    public GameObject defaultBackCamera;
+
+    public GameObject farBackCamera;
+
+    public GameObject firstPersonBackCamera;
+
     public CameraMode cameraMode;
+
+    private bool isBackCameraEnabled = false;
 
     // Update is called once per frame
     void Update()
@@ -24,6 +32,14 @@ public class ChangeCamera : MonoBehaviour
         if (Input.GetButtonDown("Camera"))
         {
             cameraMode = (CameraMode)(((int)++cameraMode) % 3);
+        }
+        if (Input.GetButtonDown("LookBack"))
+        {
+            isBackCameraEnabled = true;
+        }
+        if (Input.GetButtonUp("LookBack"))
+        {
+            isBackCameraEnabled = false;
         }
 
         StartCoroutine(ChangeMode());
@@ -35,18 +51,51 @@ public class ChangeCamera : MonoBehaviour
 
         if (cameraMode == CameraMode.DefaultCamera)
         {
-            defaultCamera.SetActive(true);
+            if (isBackCameraEnabled)
+            {
+                defaultBackCamera.SetActive(true);
+                defaultCamera.SetActive(false);
+            }
+            else
+            {
+                defaultCamera.SetActive(true);
+                defaultBackCamera.SetActive(false);
+            }
+
             firstPersonCamera.SetActive(false);
+            firstPersonBackCamera.SetActive(false);
         }
         else if (cameraMode == CameraMode.FarCamera)
         {
-            farCamera.SetActive(true);
+            if (isBackCameraEnabled)
+            {
+                farBackCamera.SetActive(true);
+                farCamera.SetActive(false);
+            }
+            else
+            {
+                farCamera.SetActive(true);
+                farBackCamera.SetActive(false);
+            }
+
             defaultCamera.SetActive(false);
+            defaultBackCamera.SetActive(false);
         }
         else if (cameraMode == CameraMode.FirstPersonCamera)
         {
-            firstPersonCamera.SetActive(true);
+            if (isBackCameraEnabled)
+            {
+                firstPersonBackCamera.SetActive(true);
+                firstPersonCamera.SetActive(false);
+            }
+            else
+            {
+                firstPersonCamera.SetActive(true);
+                firstPersonBackCamera.SetActive(false);
+            }
+
             farCamera.SetActive(false);
+            farBackCamera.SetActive(false);
         }
     }
 }
